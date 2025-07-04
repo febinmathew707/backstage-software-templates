23cfcde60180c10caf4f338dcbed7b5fa70e4747
febinmathew707 <febinmathew707@gmail.com>
2025-07-04 18:21:08 +0530

    Update app.py

--- python-app/template/app.py
+++ python-app/template/app.py
@@ -47,5 +47,5 @@ def home():
     })
 
 if __name__ == "__main__":
-    port = int(os.getenv('PORT', 8080))
+    port = int(os.getenv('PORT', 8082))
     app.run(host='0.0.0.0', port=port, debug=False) 
